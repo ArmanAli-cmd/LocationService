9a45e83a35bf4eb2a3e02e2b6602d59e49637584
hp <hp@DESKTOP-FMO2JN9>
2023-07-18 12:29:16 +0530

    Add project files.

---
+++ LocationService/Domain/Aggregates/LocationAggregate/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace LocationService.Domain.Aggregates.LocationAggregate
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
---
+++ LocationService/Infrastructure/Data/Configurations/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LocationService.Domain.Aggregates.LocationAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocationService.Infrastructure.Data.Configurations
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Location> Locations { get; set; }
+    
+    }
+}
